453100fd54e4fa252e7d2705d813a1a1c996fb8b
Jean-Louis Mainguy <jean-louis.mainguy@cossette.com>
2023-01-26 23:11:28 -0500

    Added bibliography link element

--- config/database.js
+++ config/database.js
@@ -2,17 +2,10 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres', 
+    client: 'sqlite',
     connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', ''),
-          password: env('DATABASE_PASSWORD', ''),
-          ssl: {
-            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
+  },
 });
